4cbf5467899b897fd6d432789acfda2c713b6398
Srikanth Kola <97477121+srikanth716@users.noreply.github.com>
2024-09-11 15:54:31 +0530

    [INJIVER-613] implementing zing library (#155)

--- inji-verify/src/components/Home/VerificationSection/QrScanner.tsx
+++ inji-verify/src/components/Home/VerificationSection/QrScanner.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CameraAccessDenied from "./CameraAccessDenied";
 import { ScanSessionExpiryTime } from "../../../utils/config";
 import { useAppDispatch } from "../../../redux/hooks";
@@ -8,34 +8,88 @@ import {
 } from "../../../redux/features/verification/verification.slice";
 import { raiseAlert } from "../../../redux/features/alerts/alerts.slice";
 import "./ScanningLine.css";
-import { initiateQrScanning, terminateScanning } from "../../../utils/qr-utils";
+import { BrowserMultiFormatReader, NotFoundException } from "@zxing/library";
 
 let timer: NodeJS.Timeout;
 
 function QrScanner() {
   const dispatch = useAppDispatch();
   const [isCameraBlocked, setIsCameraBlocked] = useState(false);
-
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isScanning, setIsScanning] = useState<boolean>(false);
+  const codeReader = useRef<BrowserMultiFormatReader | null>(null);
   const scannerRef = useRef<HTMLDivElement>(null);
 
-  const onSuccess = useCallback(
-    (decodedText: any) => {
-      dispatch(
-        verificationInit({
-          qrReadResult: { qrData: decodedText, status: "SUCCESS" },
-          flow: "SCAN",
-        })
-      );
-      clearTimeout(timer);
-    },
-    [dispatch]
-  );
+  // Start the scanner
+  const startScanner = async () => {
+    if (!codeReader.current || isScanning) return;
+    setIsScanning(true);
 
-  const onError = (e: any) => {
-    console.error("Error occurred:", e);
-    setIsCameraBlocked(true);
-    clearTimeout(timer);
+    try {
+      const devices = await codeReader.current.getVideoInputDevices();
+      if (devices.length === 0) {
+        console.error("No camera devices found.");
+        setIsCameraBlocked(true);
+        setIsScanning(false);
+        return;
+      }
+
+      // Select the first camera device
+      // Find the back camera (if available)
+      const backCamera = devices.find(
+        (device) =>
+          device.label.toLowerCase().includes("back") ||
+          device.label.toLowerCase().includes("rear")
+      );
+      const firstDeviceId = backCamera
+        ? backCamera.deviceId
+        : devices[0].deviceId;
+
+      await codeReader.current.decodeFromVideoDevice(
+        firstDeviceId,
+        videoRef.current!,
+        (result, err) => {
+          if (result) {
+            dispatch(
+              verificationInit({
+                qrReadResult: { qrData: result.getText(), status: "SUCCESS" },
+                flow: "SCAN",
+              })
+            );
+            clearTimeout(timer);
+            stopScanner();
+          }
+          if (err && !(err instanceof NotFoundException)) {
+            console.error("Error occurred:", err);
+            setIsCameraBlocked(true);
+            clearTimeout(timer);
+          }
+        }
+      );
+    } catch (e) {
+      console.error("Failed to start the scanner: " + e);
+      setIsCameraBlocked(true);
+      setIsScanning(false);
+    }
   };
+
+  // Stop the scanner
+  const stopScanner = () => {
+    if (codeReader.current) {
+      codeReader.current.reset();
+      setIsScanning(false);
+    }
+  };
+
+  useEffect(() => {
+    // Initialize the code reader when the component mounts
+    codeReader.current = new BrowserMultiFormatReader();
+    startScanner();
+    return () => {
+      // Cleanup: stop the scanner when the component unmounts
+      stopScanner();
+    };
+  }, []);
 
   useEffect(() => {
     timer = setTimeout(() => {
@@ -48,14 +102,13 @@ function QrScanner() {
           severity: "error",
         })
       );
-      terminateScanning();
+      stopScanner();
     }, ScanSessionExpiryTime);
-    initiateQrScanning(onSuccess, onError);
     return () => {
       console.log("Clearing timeout");
       clearTimeout(timer);
     };
-  }, [dispatch, onSuccess]);
+  }, [dispatch]);
 
   useEffect(() => {
     // Disable inbuilt border around the video
@@ -67,20 +120,37 @@ function QrScanner() {
     }
   }, [scannerRef]);
 
-  window.onpopstate = () => console.log("browser back===>>");
-
   return (
-    <div ref={scannerRef} className="relative">
+    <div
+      ref={scannerRef}
+      className="fixed inset-0 flex items-center justify-center overflow-hidden lg:relative lg:overflow-visible"
+    >
       {!isCameraBlocked && (
         <div className="absolute top-[-15px] left-[-15px] h-[280px] w-[280px] lg:top-[-12px] lg:left-[-12px] lg:h-[340px] lg:w-[340px] flex items-center justify-center">
-          <div id="scanning-line" className="scanning-line"></div>
+          <div
+            id="scanning-line"
+            className="hidden lg:block scanning-line"
+          ></div>
         </div>
       )}
 
-      <div
-        className="none relative h-[250px] w-[250px] lg:h-[316px] lg:w-[316px] rounded-lg overflow-hidden flex items-center justify-center"
-        id="reader"
-      />
+      <div className="relative h-screen w-screen lg:h-[316px] lg:w-[316px] rounded-lg overflow-hidden flex items-center justify-center z-0">
+        <button
+          onClick={() => {
+            stopScanner();
+            dispatch(goHomeScreen({}));
+          }}
+          className="absolute top-4 right-4 lg:hidden bg-gray-800 text-white p-2 rounded-full hover:bg-gray-700 focus:outline-none z-20"
+          aria-label="Close Scanner"
+        >
+          ✕
+        </button>
+
+        <video
+          ref={videoRef}
+          className="h-full w-full object-cover rounded-lg"
+        />
+      </div>
 
       <CameraAccessDenied
         open={isCameraBlocked}
